292671d92e778a9de84c8f3b7508f2cb64db2c2f
Zhicong Zhang <a2512878126@gmail.com>
2020-02-15 14:41:32 +0800

    free has been slain

--- project/static/js/freeRead.js
+++ project/static/js/freeRead.js
@@ -1,3 +1,30 @@
-$(document).ready(function(){
-    
-});
+var freeService = {
+    api: {},
+}
+var freeHandler = {
+    args: {},
+    init: function() {
+        console.log("payfreeHandlerInit")
+        // 载入menu
+        loadPage($("#menu"), "../component/menu.html", function() {
+            $(".menu li").eq(3).addClass("active")
+            menuTouch()
+        });
+        var freeSystemSwiper = new Swiper('.free-system-swiper-container', {
+            loop: true,
+            autoplay: {
+                delay: 2000,
+                disableOnInteraction: false,
+            },
+            pagination: {
+                el: '.free-system-swiper-pagination',
+            },
+        })
+    },
+    initAction: function() {
+    },
+}
+$(function() {
+    freeHandler.init();
+    freeHandler.initAction();
+})
