92b333b7996f788aa575bf41f62bea9ba1763906
张智聪 <zhangzhicong@rongyiju.com>
2020-02-25 10:37:45 +0800

    fix first load

--- project/static/js/common/common.js
+++ project/static/js/common/common.js
@@ -6,23 +6,6 @@ var _hmt = _hmt || [];
     s.parentNode.insertBefore(hm, s);
 })();
 
-(function (doc, win) {
-    var docEl = doc.documentElement,
-            resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-            recalc = function () {
-                var clientWidth = docEl.clientWidth;
-                if (!clientWidth) return;
-                if (clientWidth >= 750) {
-                    docEl.style.fontSize = '100px';
-                } else {
-                    docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
-                }
-            };
-    if (!doc.addEventListener) return;
-    win.addEventListener(resizeEvt, recalc, false);
-    recalc()
-})(document, window);
-
 function menuTouch() {
     var startX = 0
     var endX = 0
